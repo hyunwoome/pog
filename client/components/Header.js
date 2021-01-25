bc6e6980dedf575ede4df57d6f0621bc4705a68f
hyunwoome <hyunwoome@icloud.com>
2021-01-25 14:57:10 +0900

    Client: Complete Navbar UI

---
+++ client/components/Header.js
@@ -0,0 +1,40 @@
+import styles from './Header.module.css';
+import SearchIcon from '@material-ui/icons/Search';
+import MenuIcon from '@material-ui/icons/Menu';
+import GolfCourseIcon from '@material-ui/icons/GolfCourse';
+import Image from 'next/image';
+
+const Header = () => {
+	return (
+		<header className={styles.container}>
+			<section className={styles.navbar}>
+				<div className={styles.logo}>
+					<a href="/">
+						<GolfCourseIcon />
+						<h3>Pride of Golf</h3>
+					</a>
+				</div>
+				<div className={styles.search}>
+					<form className={styles.searchBar} action="">
+						<input
+							type="text"
+							placeholder="해외골프는 POG와 함께"
+							name="search"
+						/>
+						<button type="submit">
+							<SearchIcon />
+						</button>
+					</form>
+				</div>
+				<div className={styles.menu}>
+					<button>
+						<MenuIcon />
+					</button>
+				</div>
+			</section>
+			<section className={styles.mainImage}></section>
+		</header>
+	);
+};
+
+export default Header;
