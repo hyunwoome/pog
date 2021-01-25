ac35320de4517086649a5cfaec964876e2e53d73
hyunwoome <hyunwoome@icloud.com>
2021-01-26 00:40:58 +0900

    Client: Add Main Image

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,5 @@
 import Header from '../components/Header';
+import Main from '../components/Main';
 import Menu from '../components/Menu';
 import styles from '../styles/Home.module.css';
 
@@ -6,6 +7,7 @@ export default function Home() {
 	return (
 		<>
 			<Header />
+			<Main />
 			<Menu />
 		</>
 	);
