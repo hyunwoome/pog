e83b6a40680823c5b1a24ab546501f4151edadb4
hyunwoome <hyunwoome@icloud.com>
2021-01-28 13:35:37 +0900

    Client: Implementing Header menu Toogle

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,8 +1,6 @@
 import styles from './Header.module.css';
 import Link from 'next/link';
 import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import { blue, lightBlue } from '@material-ui/core/colors';
 import { useState } from 'react';
 
 const Header = () => {
@@ -12,14 +10,6 @@ const Header = () => {
 		setShow(!show);
 	};
 
-	const onBlurToggle = () => {
-		if (show === false) {
-			setShow(show);
-		} else {
-			setShow(!show);
-		}
-	};
-
 	return (
 		<section className={styles.headerContainer}>
 			<div className={styles.logo}>
@@ -27,35 +17,31 @@ const Header = () => {
 					<h1>Pride of Golf</h1>
 				</Link>
 			</div>
-			{/* {show === true ? ( */}
-			<div className={styles.navbar}>
-				<div>
-					<Link href="/login">
-						<a>로그인</a>
-					</Link>
+			{show === true ? (
+				<div className={styles.navbar}>
+					<div>
+						<Link href="/login">
+							<a className={styles.login}>로그인</a>
+						</Link>
+					</div>
+					<li>
+						<Link href="/">
+							<a>예약확인</a>
+						</Link>
+					</li>
+					<li>
+						<Link href="/">
+							<a>견적문의</a>
+						</Link>
+					</li>
+					<li>
+						<Link href="/">
+							<a>후기게시판</a>
+						</Link>
+					</li>
 				</div>
-				<li>
-					<Link href="/">
-						<a>예약확인</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/">
-						<a>견적문의</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/">
-						<a>후기게시판</a>
-					</Link>
-				</li>
-			</div>
-			{/* ) : null */}
-			<button
-				className={styles.menuIcon}
-				onClick={onClickToggle}
-				onBlur={onBlurToggle}
-			>
+			) : null}
+			<button className={styles.menuIcon} onClick={onClickToggle}>
 				<MenuIcon />
 			</button>
 		</section>
