fe5cd6c7c1a5dea0d07a3b35ed6e043d433f2d06
hyunwoome <hyunwoome@icloud.com>
2021-01-30 13:36:11 +0900

    Client: Remove Carousel

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,15 @@
 import styled from 'styled-components';
-import Carousel from '../components/main/Carousel';
 import Tabs from '../components/main/Tabs';
 
-const Home = () => {
+export default function Home() {
 	return (
 		<HomeContainer>
 			<HomeWrapper>
-				<Carousel />
 				<Tabs />
 			</HomeWrapper>
 		</HomeContainer>
 	);
-};
+}
 
 const HomeContainer = styled.div`
 	width: 100%;
@@ -22,5 +20,3 @@ const HomeContainer = styled.div`
 const HomeWrapper = styled.div`
 	width: 100%;
 `;
-
-export default Home;
